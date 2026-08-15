0627839bdff3afcdecbd25905d317e79a289fbc0
RohanLabde <46919422+RohanLabde@users.noreply.github.com>
2026-08-15 16:09:22 +0530

    Update fpl_data.py

--- data/fpl_data.py
+++ data/fpl_data.py
@@ -3,7 +3,34 @@ import pandas as pd
 
 def get_players(data):
 
-    return pd.DataFrame(data["elements"])
+    players = pd.DataFrame(data["elements"])
+
+    teams = pd.DataFrame(data["teams"])
+    positions = pd.DataFrame(data["element_types"])
+
+    team_map = dict(
+        zip(
+            teams["id"],
+            teams["name"]
+        )
+    )
+
+    position_map = dict(
+        zip(
+            positions["id"],
+            positions["singular_name"]
+        )
+    )
+
+    players["team_name"] = players["team"].map(team_map)
+
+    players["position"] = players["element_type"].map(
+        position_map
+    )
+
+    players["price"] = players["now_cost"] / 10
+
+    return players
 
 
 def get_teams(data):
@@ -13,4 +40,4 @@ def get_teams(data):
 
 def get_fixtures(data):
 
-    return pd.DataFrame(data["fixtures"])
+    return pd.DataFrame(data)
