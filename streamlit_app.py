461c30c9da0d90fd94b22a1671adccdcfa98516d
RohanLabde <46919422+RohanLabde@users.noreply.github.com>
2026-08-15 16:10:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-from data.fpl_api import get_fpl_data
-from data.fpl_data import get_players, get_teams, get_fixtures
+from data.fpl_api import get_fpl_data, get_fixtures
+from data.fpl_data import get_players, get_teams, get_fixtures as clean_fixtures
 
 
 st.set_page_config(
@@ -17,14 +17,20 @@ st.subheader("FPL Data Explorer")
 
 try:
 
+    # Get main FPL data
     data = get_fpl_data()
 
+    # Get fixtures separately
+    fixture_data = get_fixtures()
+
+    # Clean data
     players = get_players(data)
     teams = get_teams(data)
-    fixtures = get_fixtures(data)
+    fixtures = clean_fixtures(fixture_data)
 
     st.success("FPL data loaded successfully!")
 
+    # Metrics
     col1, col2, col3 = st.columns(3)
 
     col1.metric(
@@ -42,10 +48,23 @@ try:
         len(fixtures)
     )
 
+    # Player table
     st.subheader("Players")
 
     st.dataframe(
-        players,
+        players[
+            [
+                "id",
+                "first_name",
+                "second_name",
+                "team_name",
+                "position",
+                "price",
+                "total_points",
+                "form",
+                "selected_by_percent"
+            ]
+        ],
         use_container_width=True
     )
 
