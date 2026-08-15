406464918283e0e9dc3ca9e12a9d76803ab0bac7
RohanLabde <46919422+RohanLabde@users.noreply.github.com>
2026-08-15 15:30:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.set_page_config(
+    page_title="FPL AI Coach",
+    page_icon="⚽",
+    layout="wide"
 )
+
+st.title("⚽ FPL AI Coach")
+
+st.subheader("Your personal FPL decision engine")
+
+st.write(
+    "This application will analyse my FPL team "
+    "and recommend the best transfers."
+)
+
+st.success("Application is running!")
