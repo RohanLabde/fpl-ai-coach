5f090998b24d1673e59513e675e41ace9adeb32f
RohanLabde <46919422+RohanLabde@users.noreply.github.com>
2026-08-15 16:08:28 +0530

    Update fpl_api.py

--- data/fpl_api.py
+++ data/fpl_api.py
@@ -2,10 +2,27 @@ import requests
 
 
 FPL_BOOTSTRAP_URL = "https://fantasy.premierleague.com/api/bootstrap-static/"
+FPL_FIXTURES_URL = "https://fantasy.premierleague.com/api/fixtures/"
 
 
 def get_fpl_data():
-    response = requests.get(FPL_BOOTSTRAP_URL, timeout=30)
+
+    response = requests.get(
+        FPL_BOOTSTRAP_URL,
+        timeout=30
+    )
+
+    response.raise_for_status()
+
+    return response.json()
+
+
+def get_fixtures():
+
+    response = requests.get(
+        FPL_FIXTURES_URL,
+        timeout=30
+    )
 
     response.raise_for_status()
 
