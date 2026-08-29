656c0d6e0f47e95de1916854b5e5c9da08d2bf56
RohanLabde <46919422+RohanLabde@users.noreply.github.com>
2026-08-29 13:47:17 +0530

    Remove unused import of build_team_gameweek_calendar

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,7 @@ HISTORICAL_2025_26_URL = (
 from data.fpl_api import get_fpl_data, get_fixtures
 from data.fpl_data import get_players
 from data.features import (
-    build_form_features,
-    build_team_gameweek_calendar
+    build_form_features    
 )
 
 
